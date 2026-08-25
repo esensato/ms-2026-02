a1a2063f021cb7f76aaf4fc3c9af119b952e152f
Edson Alexandre Sensato <edson.sensato@espm.br>
2026-08-24 22:33:59 -0300

    24/08

---
+++ aula/aluno-restful/src/main/java/aula/microservicos/aluno/restful/Aluno.java
@@ -0,0 +1,13 @@
+package aula.microservicos.aluno.restful;
+
+import java.util.Date;
+
+public class Aluno {
+
+    public Integer id;
+    public String nome;
+    public String curso;
+    public String telefone;
+    public Date dataNascimento;
+    
+}
---
+++ aula/aluno-restful/src/main/java/aula/microservicos/aluno/restful/AlunoController.java
@@ -0,0 +1,37 @@
+package aula.microservicos.aluno.restful;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/aluno")
+public class AlunoController {
+
+    // http://localhost:8080/aluno/100 -> retorna aluno com id = 100
+    @GetMapping("{id}")
+    public ResponseEntity<Aluno> getAluno(@PathVariable Integer id) {
+        Aluno teste = new Aluno();
+        teste.id = 100;
+        teste.nome = "Joao da Silva";
+        teste.dataNascimento = new Date();
+        teste.curso = "CDN";
+        teste.telefone = "2344-342324";
+        
+        return new ResponseEntity<Aluno>(teste, HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Aluno> cadastro(@RequestBody Aluno aluno) {
+        return new ResponseEntity<Aluno>(aluno, HttpStatus.OK);
+    }
+    
+}
